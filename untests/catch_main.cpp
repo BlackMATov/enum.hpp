b3706767659990e333b7d06eb9b7d998f4cebbc1
BlackMATov <blackmat@mail.ru>
2020-03-03 00:46:15 +0700

    Happy New Year 🥳

--- untests/catch_main.cpp
+++ untests/catch_main.cpp
@@ -1,7 +1,7 @@
 /*******************************************************************************
  * This file is part of the "https://github.com/blackmatov/enum.hpp"
  * For conditions of distribution and use, see copyright notice in LICENSE.md
- * Copyright (C) 2019 Matvey Cherevko
+ * Copyright (C) 2019-2020, by Matvey Cherevko (blackmatov@gmail.com)
  ******************************************************************************/
 
 #define CATCH_CONFIG_MAIN
